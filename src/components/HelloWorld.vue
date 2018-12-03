bef63cc19c894f833bbf2b63e30806065fdbcf7e
Jake Overall <joverall22@gmail.com>
2018-12-03 12:04:35 -0700

    slap build

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id='hello-world'>
+    {{swapiPeople}}
+    <!-- <div v-for="target in targets"> -->
+    <h1>Hello my name is {{target.name}}</h1>
+    <img :src="target.img">
+    <p :class="{low: checkLow()}">Health: {{target.health}}</p>
+    <p v-if="target.health == 0">He's Sleeping</p>
+    <button @click='attack(1)'>SLAP</button>
+    <button @click='attack(5)'>Punch</button>
+    <button @click='attack(10)'>Kick</button>
+    <!-- </div> -->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+      target() {
+        return this.$store.state.targets[0]
+      },
+      swapiPeople() {
+        return this.$store.state.swapiPeople
+      }
+    },
+    methods: {
+      attack(val) {
+        //dispatch invokes an action by its string name
+        this.$store.dispatch('attack', val)
+      },
+      checkLow() {
+        return this.target.health < 51
+      }
+    }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  img {
+    height: 200px
+  }
+
+  .low {
+    color: orange
+  }
+
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios';
+
+let api = Axios.create({
+  baseURL: 'https://swapi.co/api/'
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    targets: [{
+      name: 'Jake',
+      img: "http://jakeoverall.github.io/resume/assets/img/jake-sm.jpg",
+      health: 100
+    }, {
+      name: 'Bob',
+      img: "http://vignette3.wikia.nocookie.net/mycun-the-movie/images/c/c0/Bob.jpeg/revision/latest/scale-to-width-down/304?cb=20160110034246",
+      health: 120
+    }, {
+      name: "Steve",
+      img: "https://i.redd.it/e727iwai0i111.jpg",
+      health: 70
+    }],
+    swapiPeople: []
   },
   mutations: {
-
+    //first value on every mutation is state
+    attack(state, hitValue) {
+      state.targets[0].health -= hitValue
+    },
+    addPeople(state, people) {
+      state.swapiPeople = people
+    }
   },
   actions: {
-
+    //first parameter on every action is {commit, dispatch}
+    attack({ commit, dispatch }, hitValue) {
+      api.get('people')
+        .then(res => {
+          commit('addPeople', res.data.results)
+          commit('attack', hitValue)
+        })
+    }
   }
 })
